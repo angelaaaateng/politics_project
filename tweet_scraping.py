569885782f48f24a443b954df8e9bc22c723e6f7
Amber Teng - windows <31015696+angelaaaateng@users.noreply.github.com>
2022-10-06 17:04:22 +0800

    Update tweet_scraping.py

--- tweet_scraping.py
+++ tweet_scraping.py
@@ -44,6 +44,8 @@ from pprint import pprint
 from wordcloud import STOPWORDS
 stopwords = set(STOPWORDS)
 
+import time
+
 
 nlp = spacy.load('en_core_web_lg')
 
@@ -52,5 +54,25 @@ st.title('Tweet Scraper')
 
 # read tweets from before 
 senator_tweet_df = pd.read_csv('C:/Users/Admin/Documents/GitHub/politics_project/senator_tweets_df.csv')
+
+# st.dataframe(data=senator_tweet_df)
+
+st.write(senator_tweet_df.shape)
+
+print("Running reading scores... ")
+
+my_bar = st.progress(0)
+senator_tweet_df['flesch'] = senator_tweet_df['tweet_content'].apply(lambda s:textstat.flesch_reading_ease(s))
+my_bar.progress(1/6)
+senator_tweet_df['flesch_grade'] = senator_tweet_df['tweet_content'].apply(lambda s:textstat.flesch_kincaid_grade(s))
+my_bar.progress(2/6)
+senator_tweet_df['ari'] = senator_tweet_df['tweet_content'].apply(lambda s:textstat.automated_readability_index(s))
+my_bar.progress(3/6)
+senator_tweet_df['dale_chall'] = senator_tweet_df['tweet_content'].apply(lambda s:textstat.dale_chall_readability_score(s))
+my_bar.progress(4/6)
+senator_tweet_df['readability_consensus'] = senator_tweet_df['tweet_content'].apply(lambda s:textstat.text_standard(s))
+my_bar.progress(5/6)
+senator_tweet_df['reading_time'] = senator_tweet_df['tweet_content'].apply(lambda s:textstat.reading_time(s))
+my_bar.progress(6/6)
 
 st.dataframe(data=senator_tweet_df)
